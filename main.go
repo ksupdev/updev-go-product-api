1d5f2bac6affb65e938766f83db117c11d7e86d7
Karoon Sillapapan <karoon@ananda.co.th>
2021-03-25 14:15:09 +0700

    Implement goodbye

--- main.go
+++ main.go
@@ -12,8 +12,11 @@ func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	hh := handlers.NewHello(l)
 
+	gh := handlers.NewGoodbye(l)
+
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
 
 	http.ListenAndServe(":9090", sm)
 }
